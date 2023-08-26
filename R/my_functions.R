4211b3e080bbd155cd6756df4f13ea97f13b82df
Guilherme Marques <guizao@MacBook-Pro-de-Guilherme.local>
2023-08-26 00:54:06 -0300

    first commit

---
+++ R/my_functions.R
@@ -0,0 +1,12 @@
+#' Soma dois números.
+#'
+#' @param x Um número.
+#' @param y Outro número.
+#' @return A soma de x e y.
+#'
+#' @examples
+#' soma(2, 3)
+#'
+soma <- function(x, y) {
+  return(x + y)
+}
